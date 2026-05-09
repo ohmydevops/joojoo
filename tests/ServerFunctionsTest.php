7db4bf3a44ff00cbca1476bf0cd94377b26b5bb5
ohmydevops <ohmydevops@gmail.com>
2026-05-09 12:40:37 +0330

    feat: refactor server structure by moving functions to server_lib.php and updating test dependencies

--- tests/ServerFunctionsTest.php
+++ tests/ServerFunctionsTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__ . '/../server.php';
+require_once __DIR__ . '/../src/server_lib.php';
 
 final class ServerFunctionsTest extends TestCase
 {
